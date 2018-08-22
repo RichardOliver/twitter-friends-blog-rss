98a18ea1e6ee4cb004c69ce20c8814062e76f314
Oliver <richard.oliver@aspect.com>
2018-08-22 22:42:38 +0100

    tidy up and add code to run find rss

--- src/app.js
+++ src/app.js
@@ -1,28 +1,77 @@
-import Twitter from 'twitter';
-import Finder from 'find-rss';
+var Twitter = require('twitter');
+var Finder = require('find-rss');
 
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+ const app = {
+  _client: null,
+  _screenName: "",
+  _urls: [],
 
-var urls = [];
-var params = {
-                screen_name: '',
-                cursor: -1,
-                count: 20,
-                skip_status: true,
-                include_user_entities: true,
-             };
-
-client.get('friends/list', params, function(error, users, response) {
-  if (!error) {
-    users.users.forEach(function(user){
-        slimUser = (({ screen_name, url }) => ({ screen_name, url }))(user)
-        console.log(slimUser);
+  init: function(screenName) {
+    _client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
     });
+
+    Finder.setOptions({
+      favicon: false,
+      getDetail: true
+    });
+
+    _screenName = screenName;
+    this.getFriends(_screenName, "-1")
+  },
+
+  getFriends: function(screenName, cursor) {
+    let params = {
+      screen_name: screenName,
+      cursor: cursor,
+      count: 20,
+      skip_status: true,
+      include_user_entities: true,
+    };
+
+    _client.get('friends/list', params, this.processFriends.bind(this));
+  },
+
+  processFriends: function(error, results, response) {
+    if (!error) {
+      let that = this;
+      results.users.forEach(function(user){
+          //slimUser = (({ screen_name, url }) => ({ screen_name, url }))(user)
+          //console.log(slimUser);
+          if (user.url != null) {
+            that.getFeeds(user.url);
+          }
+      });
+
+      if (results.next_cursor_str !== "0"){
+        this.getFriends(_screenName, results.next_cursor_str);
+      }
+    }
+    else {
+      console.log(error); 
+    }
+
+  },
+
+  getFeeds: function(url) {
+    Finder(url, this.processFeeds.bind(this));
+  },
+
+  processFeeds(error, response, body) {
+    console.log("----------------------------");
+
+    if (!error) {
+      console.log(response);
+    }
+    else {
+      console.log(error); 
+    }
   }
-  else { console.log(error); }
-});
+
+
+};
+
+app.init('rolivercoffee');
